0e93023a50cbbd9dfcabdc3958a88f3dce728289
Dennis <18040598+data-intelligence-analysis@users.noreply.github.com>
2020-12-20 21:56:36 -0600

    Changes to the website directories

---
+++ Portfolio/js/index.js
@@ -0,0 +1,15 @@
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link')
+
+/*Everytime we click on the button it should add the class*/
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+/*Links to different sections within the page*/
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        /*The navigation menu closes when you click on different sections within the page*/
+        document.body.classList.remove('nav-open');
+    })
+})
